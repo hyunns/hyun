18137e7ca391738d543cb06aad900e25f4c0de9c
hyunns <gurwovos@naver.com>
2021-08-28 23:34:44 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
     gsap.to('.loading', {y: '-100%', delay: 4})
 
     $('body').mousemove(function(e) {
-        gsap.to('#cursor', { duration: 0.5 , x: e.clientX, y: e.clientY, ease: Power3.easeOut });
+        gsap.to('#cursor', { duration: 0.4 , x: e.clientX, y: e.clientY, ease: Power3.easeOut });
     });
 
     //header
@@ -19,13 +19,13 @@
     // portfolio
     const portSlider = new Swiper('.port_slider', { direction: 'vertical', slidesPerView: 1, mousewheel: true, });
 
+    let port = gsap.timeline();
+    port.fromTo('.swiper-slide .detail_page', {y: '40px', autoAlpha: 0}, {y: 0, autoAlpha:1, duration: 1,}, 4)
+    port.to('.swiper-slide .line_grow', { width: '100%', duration: 1.2, }, '-=1')
+    port.fromTo('.swiper-slide .txt_wrap *', {y: '40px', autoAlpha: 0,}, {duration:1, y: 0, autoAlpha:1, stagger:0.15}, 4)
+
     // let port1 = gsap.timeline();
     // port1.fromTo('.port01 .detail_page', {y: '20px', autoAlpha: 0}, {y: 0, autoAlpha:1, duration: 1,})
     // port1.to('.port01 .line_grow', {width:'100%', left: 0, duration: 1},'-=1')
     // port1.fromTo('.port01 .txt_wrap *', {y: '50px', autoAlpha: 0,},{duration:1, y: 0, autoAlpha:1, stagger:0.15})
-
-    let port = gsap.timeline();
-    port.fromTo('.swiper-slide .detail_page', {y: '20px', autoAlpha: 0}, {y: 0, autoAlpha:1, duration: 1,},4)
-    port.to('.swiper-slide .line_grow', {width:'100%', left: 0, duration: 1},'-=1')
-    port.fromTo('.swiper-slide .txt_wrap *', {y: '50px', autoAlpha: 0,},{duration:1, y: 0, autoAlpha:1, stagger:0.15},4)
 })();
