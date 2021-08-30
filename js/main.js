6de355c1ffa3ee7cc29d6da331bf46d9d9af20e5
hyunns <gurwovos@naver.com>
2021-08-30 15:47:22 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,64 +1,50 @@
 (function() {
     gsap.registerPlugin(ScrollTrigger);
     
-    // loading
+    // loading screen
     gsap.to('.loading', {y: '-100%', delay: 4})
+    
+    // mouse effect
+    $('body').mousemove(function(e) { gsap.to('#cursor', { duration: 0.4 , x: e.clientX, y: e.clientY, ease: Power3.easeOut }) });
 
-    $('body').mousemove(function(e) {
-        gsap.to('#cursor', { duration: 0.4 , x: e.clientX, y: e.clientY, ease: Power3.easeOut });
-    });
-
-    $('body a').mousemove(function () {
-        gsap.to('.cursor_circle', 0.4, {
-            width: 50,
-            height: 50,
-            'background-color': 'rgb(110, 68, 174, 0.6)',
-            border: 'none',
-        })
+    $('.img_wrap').mousemove(function() {
+        gsap.to('#cursor', 0.4, { width: 100, height: 100 })
+        gsap.to('#cursor svg', { visibility: 'visible', opacity: 1,})
     })
-    $('body a').mouseleave(function () {
-        gsap.to('.cursor_circle', 0.4, {
-            width: 30,
-            height: 30,
-            'background-color': 'transparent',
-            border: '1px solid rgb(110, 68, 174)',
-        })
+    $('.img_wrap').mouseleave(function() {
+        gsap.to('#cursor svg', { visibility: 'hidden', opacity: 0,})
+        gsap.to('#cursor', 0.4, { width: 30, height: 30, })
     })
 
     //header
-    gsap.fromTo('header .logo', {y: '-100%', autoAlpha: 0}, {y: '-50%', autoAlpha:1, duration: 0.5, delay: 4.5})
-    gsap.fromTo('header .txt_wrap *', {y: '-100%', autoAlpha: 0,}, {y: 0, autoAlpha: 1, duration: 0.5, stagger:0.2, delay: 5})
-
-    // spin    
-    let spin = gsap.timeline({ scrollTrigger: { trigger: '.port_slider', start: 'top', scrub: 1, pin: true}})
-    spin.to('.spin', {rotation: -120, duration: 1})
-
+    gsap.fromTo('header .logo', {y: '-100%', autoAlpha: 0}, {y: '-50%', autoAlpha:1, duration: 0.5, delay: 1})
+    gsap.fromTo('header .txt_wrap *', {y: '-100%', autoAlpha: 0,}, {y: 0, autoAlpha: 1, duration: 0.5, stagger:0.2, delay: 1.2})
+    
     // portfolio
+    let port = gsap.timeline();
+    port.fromTo('.swiper-slide .line_grow',{width: 0}, { width: '100%', duration: 1, delay:0.8,})
+    port.fromTo('.swiper-slide .txt_wrap *', { autoAlpha: 0, y: '100%'}, {duration:1, autoAlpha:1, y: 0, stagger: 0.15},'-=1')
+
     const portSlider = new Swiper('.port_slider', {
         direction: 'vertical',
         slidesPerView: 1,
+        speed: 1000,
         mousewheel: true,
         on: {
+            init: function() {
+
+            },
             slideChange: function() {
                 let i = this.realIndex;
-                if (i === 0 || i === 1 || i === 2) {
-                    fnPort();
-                    return false;
+                port.delay(1).restart();
+                if (i === 0) {
+                    gsap.to('.spin', {rotation: 72, duration: 1.2})
+                } else if (i === 1) {
+                    gsap.to('.spin', {rotation: -50, duration: 1.2})
+                } else {
+                    gsap.to('.spin', {rotation: -165, duration: 1.2})
                 }
             }
         }
     });
-
-    // 슬라이드 활성화 시 효과
-    // let port = gsap.timeline();
-    // port.fromTo('.swiper-slide-active .detail_page', {y: '40px', autoAlpha: 0}, {y: 0, autoAlpha:1, duration: 1,}, 4)
-    // port.to('.swiper-slide-active .line_grow', { width: '100%', duration: 1.2, }, '-=1')
-    // port.fromTo('.swiper-slide-active .txt_wrap *', {y: '40px', autoAlpha: 0,}, {duration:1, y: 0, autoAlpha:1, stagger:0.15}, 4)
-
-    function fnPort() {
-        let port = gsap.timeline();
-        port.fromTo('.swiper-slide .line_grow',{width: 0}, { width: '100%', duration: 1})
-        port.fromTo('.swiper-slide .txt_wrap *', { autoAlpha: 0, y: '100%'}, {duration:1,autoAlpha:1, y: 0, stagger: 0.15},'-=1')
-        port.fromTo('.swiper-slide .img_wrap', {y: 20, autoAlpha: 0}, {y: 0, autoAlpha:1, duration: 0.8,}, 1.5)
-    }
 })();
