b1c4d5061672d46d0e2e13e3b669a862ad06db0b
hyunns <gurwovos@naver.com>
2021-09-01 21:49:06 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -20,41 +20,31 @@
     gsap.fromTo('header .top_header', {y: '-100%', autoAlpha: 0}, {y: '0', autoAlpha:1, duration: 0.8, delay: 4})
     gsap.fromTo('.intro .tit_wrap *', { autoAlpha: 0, }, { autoAlpha: 1, 'letter-spacing':'0.05em', duration: 1},4.5)
     
-    // portfolio
-    let port = gsap.timeline();
-    port.fromTo('.swiper-slide .line_grow',{width: 0}, { width: '100%', duration: 1, delay:0.8,})
-    port.to('.swiper-slide .txt_wrap', {duration: 0.5, autoAlpha: 1,'background-color':'#080808'},'-=1')
-    port.fromTo('.swiper-slide .txt_wrap *', { autoAlpha: 0, y: '100%'}, {duration:1, autoAlpha:1, y: 0, stagger: 0.15, 'background-color':'#080808'},'-=1')
+    // contents
+    let content = gsap.timeline({ scrollTrigger: { trigger: ".contents", start: "top top", end: "+=3000", scrub: 1, pin: true,} });
+    content.to('.contents ul', {duration: 1.2, y: '-33.33%',delay: 1})
+    .to('.contents ul', {duration: 1.2, y: '-66.66%',delay: 1})
 
-    const portSlider = new Swiper('.port_slider', {
-        direction: 'vertical',
-        slidesPerView: 1,
-        speed: 1000,
-        mousewheel: true,
-        on: {
-            init: function() {
-                gsap.to('.spin01', {'fill': 'rgb(255, 255, 255, 0.8)'})
-            },
-            slideChange: function() {
-                let i = this.realIndex;
-                port.delay(1).restart();
-                if (i === 0) {
-                    gsap.to('.spin', {rotation: 72, duration: 1.2})
-                    gsap.to('.spin01', {'fill': 'rgb(255, 255, 255, 0.8)'})
-                    gsap.to('.spin02', {'fill': 'rgb(255, 255, 255, 0.2)'})
-                    gsap.to('.spin03', {'fill': 'rgb(255, 255, 255, 0.2)'})
-                } else if (i === 1) {
-                    gsap.to('.spin', {rotation: -50, duration: 1.2})
-                    gsap.to('.spin01', {'fill': 'rgb(255, 255, 255, 0.2)'})
-                    gsap.to('.spin02', {'fill': 'rgb(255, 255, 255, 0.8)'})
-                    gsap.to('.spin03', {'fill': 'rgb(255, 255, 255, 0.2)'})
-                } else {
-                    gsap.to('.spin', {rotation: -165, duration: 1.2})
-                    gsap.to('.spin01', {'fill': 'rgb(255, 255, 255, 0.2)'})
-                    gsap.to('.spin02', {'fill': 'rgb(255, 255, 255, 0.2)'})
-                    gsap.to('.spin03', {'fill': 'rgb(255, 255, 255, 0.8)'})
-                }
-            }
-        }
-    });
+    gsap.set('.spin01', {'fill': 'rgb(255, 255, 255, 0.8)'})
+
+    let circle = gsap.timeline({ scrollTrigger: { trigger: ".contents", start: "top top", end: "+=3000", scrub: 1, } });
+    circle.to('.spin', {rotation: -50, duration: 1.2, delay: 1})
+    .to('.spin01', {'fill': 'rgb(255, 255, 255, 0.2)'},'-=1')
+    .to('.spin02', {'fill': 'rgb(255, 255, 255, 0.8)'},'-=1')
+    .to('.spin03', {'fill': 'rgb(255, 255, 255, 0.2)'},'-=1')
+    .to('.spin', {rotation: -165, duration: 1.2, delay: 1})
+    .to('.spin02', {'fill': 'rgb(255, 255, 255, 0.2)'},'-=1')
+    .to('.spin03', {'fill': 'rgb(255, 255, 255, 0.8)'},'-=1')
+
+    let txt1 = gsap.timeline({ scrollTrigger: { trigger :'.contents .port01', start: "top top", end: "+=1000", scrub: 1}});
+    txt1.fromTo('.port01 .line_grow', {width:0}, {width:'100%', duration: 1, delay:1})
+    txt1.fromTo('.port01 .txt_wrap *', { autoAlpha: 0, y: '100%'}, {duration:1, autoAlpha:1, y: 0, stagger: 0.2},'-=1')
+
+    let txt2 = gsap.timeline({ scrollTrigger: { trigger :'.contents .port02', start: "top top", end: "+=1000", scrub: 1} });
+    txt2.fromTo('.port02 .line_grow', {width:0}, {width:'100%', duration: 1, delay:1})
+    txt2.fromTo('.port02 .txt_wrap *', { autoAlpha: 0, y: '100%'}, {duration:1, autoAlpha:1, y: 0, stagger: 0.2},'-=1')
+    
+    let txt3 = gsap.timeline({ scrollTrigger: { trigger :'.contents .port03', start: "top top", end: "+=2000", scrub: 1} });
+    txt3.fromTo('.port03 .line_grow', {width:0}, {width:'100%', duration: 1, delay:1})
+    txt3.fromTo('.port03 .txt_wrap *', { autoAlpha: 0, y: '100%'}, {duration:1, autoAlpha:1, y: 0, stagger: 0.2},'-=1')
 })();
